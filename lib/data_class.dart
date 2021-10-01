44425b41594655f17b86c5e381d315c1e4ea26a6
Bhavik 0 <bhaviklimbani1@gmail.com>
2021-10-01 14:04:09 +0530

    create data class

---
+++ lib/data_class.dart
@@ -0,0 +1,22 @@
+class User {
+  final int id;
+  final String name, email, gender, profile;
+
+  User(
+      {required this.id,
+      required this.name,
+      required this.email,
+      required this.gender,
+      required this.profile});
+}
+
+class CatalogModel {
+  static final items = [
+    User(
+        id: 1,
+        name: "bhavik",
+        email: "bhaviklimbani1@Gmail.com",
+        gender: "Male",
+        profile: "http://dummyimage.com/122x100.png/cc0000/ffffff")
+  ];
+}
